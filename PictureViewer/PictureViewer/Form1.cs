67ccbd6b2a7b6979faea8c6c0e352958119561d6
KienLe <23520797@gm.uit.edu.vn>
2024-09-25 13:42:17 +0700

    after add events

---
+++ PictureViewer/PictureViewer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // show the Open File dialog. If the user clicks OK, load the
+        // picture that the user chose.
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        //clear the picture
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        // show the color dialog box. If the user clicks OK, change the
+        // PictureBox control's background to the color that the user chose.
+        private void btnBackground_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }    
+        }
+
+        // close the form
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
